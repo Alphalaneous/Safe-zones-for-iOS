c883cf0cafbf17b25467a1d7b3eaaab945addf7c
Ashton <38200084+Alphalaneous@users.noreply.github.com>
2026-05-13 15:55:38 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -415,7 +415,7 @@ class $modify(MyAppDelegate, AppDelegate) {
     static AppDelegate* get() {
         auto ret = AppDelegate::get();
         ret->m_unk0ed = true;
-        log::info("size: {}", CCEGLView::get()->getVisibleSize());
+        log::info("size: {}", CCEGLView::get()->getFrameSize());
         return ret;
     }
 };
